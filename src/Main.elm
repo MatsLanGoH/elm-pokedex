8b041dc4de34685be71ce051b63751459aeba9ce
Matthias Lambrecht <matthias.lambrecht@beproud.jp>
2020-06-10 06:17:56 +0900

    Add basic query form and result view

--- src/Main.elm
+++ src/Main.elm
@@ -1,20 +1,37 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Html exposing (Html, button, div, h1, img, input, p, text)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick, onInput)
+
 
 
 ---- MODEL ----
 
 
+initial_model =
+    { query_string = ""
+    , pokemon = Pokemon "" 0 ""
+    }
+
+
 type alias Model =
-    {}
+    { query_string : String
+    , pokemon : Pokemon
+    }
+
+
+type alias Pokemon =
+    { name : String
+    , id : Int
+    , type_1 : String
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( initial_model, Cmd.none )
 
 
 
@@ -22,12 +39,20 @@ init =
 
 
 type Msg
-    = NoOp
+    = UpdateQuery String
+    | SubmitQuery
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        UpdateQuery newQuery ->
+            ( { model | query_string = newQuery }
+            , Cmd.none
+            )
+
+        SubmitQuery ->
+            Debug.todo "Add Query submission"
 
 
 
@@ -38,7 +63,25 @@ view : Model -> Html Msg
 view model =
     div []
         [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+        , h1 [] [ text "Elm Pokédex" ]
+        , viewSearchBox model
+        , viewPokemon model.pokemon
+        ]
+
+
+viewSearchBox model =
+    div []
+        [ input [ type_ "text", placeholder "Enter a Pokémon name", value model.query_string, onInput UpdateQuery ] []
+        , button [ onClick SubmitQuery ] [ text "Search" ]
+        ]
+
+
+viewPokemon pokemon =
+    div []
+        [ h1 [] [ text "Result" ]
+        , p [] [ text pokemon.name ]
+        , p [] [ text ("id: " ++ String.fromInt pokemon.id) ]
+        , p [] [ text ("type: " ++ pokemon.type_1) ]
         ]
 
 
